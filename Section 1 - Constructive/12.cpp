6cfb5bf93ad67ceb5f46b5756b906e1283b87e56
Mayur <mayurbarad2003@gmail.com>
2023-10-03 19:08:17 +0530

    Created 12.cpp

---
+++ Section 1 - Constructive/12.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int n = s.length();
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '4' || s[i] == '7')
+            cnt++;
+    }
+    if (cnt == 4 || cnt == 7)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+    return 0;
+}
