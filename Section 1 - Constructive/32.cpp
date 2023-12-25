62ed7f96df2449baa36b9cc82c8842cfea103068
Mayur <mayurbarad2003@gmail.com>
2023-12-25 18:52:16 +0530

    Created 33.cpp

---
+++ Section 1 - Constructive/32.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int k;
+    string s;
+    cin >> k >> s;
+    vector<int> v(26, 0);
+    for (auto c : s)
+    {
+        v[c - 'a']++;
+    }
+
+    string res = "";
+    for (int i = 0; i < 26; i++)
+    {
+        if (v[i] % k != 0)
+        {
+            cout << -1 << endl;
+            return 0;
+        }
+        else
+        {
+            int times = v[i] / k;
+            while (times--)
+                res.push_back('a' + i);
+        }
+    }
+    string old = res;
+    while (--k)
+    {
+        res = res + old;
+    }
+    cout << res << endl;
+    return 0;
+}
